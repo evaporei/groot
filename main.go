141c00cc891a38863c9af428a9c75afa0e47f247
Otávio Pace <otaviopp8@gmail.com>
2018-08-10 23:28:31 -0300

    cli: add command line arguments parse

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("I am groot")
+
+	err := runCli()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func runCli() (err error) {
+	if len(os.Args) > 1 {
+		gitCommand := os.Args[1]
+		gitCommandArgs := os.Args[2:]
+
+		fmt.Println(gitCommand)
+		fmt.Println(gitCommandArgs)
+
+		return nil
+	}
+
+	return errors.New("you must specify a Git command to run!")
 }
